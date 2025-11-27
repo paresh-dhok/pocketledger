81df0f1748a5ad6fcf7477a3cb27a9284c344851
paresh-dhok <pareshdhok20@gmail.com>
2025-11-27 10:55:30 +0530

    Update compileSdk to 35 to fix build error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.pocketledger"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.pocketledger"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
